de4e8373c330ddf4fd4f98d63dee057b5a71a795
eLnae <nael200906@gmail.com>
2025-12-02 11:09:12 +0700

    Update relasi.h

--- relasi.h
+++ relasi.h
@@ -1,6 +1,26 @@
-#ifndef RELASI_H_INCLUDED
-#define RELASI_H_INCLUDED
+#ifndef RELASI_H
+#define RELASI_H
 
+#include "event.h"
+#include "peserta.h"
 
+typedef struct elRelasi *adrRelasi;
 
-#endif // RELASI_H_INCLUDED
+struct elRelasi {
+    adrEvent E;
+    adrPeserta P;
+    adrRelasi next;
+};
+
+struct ListRelasi {
+    adrRelasi first;
+};
+
+void createRelasiList(ListRelasi &LR);
+adrRelasi newRelasi(adrEvent E, adrPeserta P);
+void insertRelasi(ListRelasi &LR, adrRelasi R);
+
+void showPesertaByEvent(ListRelasi LR, adrEvent E);
+void deletePesertaFromEvent(ListRelasi &LR, adrEvent E, string nama);
+
+#endif
