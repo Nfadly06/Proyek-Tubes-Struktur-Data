84f605579229fef3a00cf4f715741d27843f6196
eLnae <nael200906@gmail.com>
2025-12-02 11:05:03 +0700

    Update event.h

--- event.h
+++ event.h
@@ -1,6 +1,36 @@
-#ifndef EVENT_H_INCLUDED
-#define EVENT_H_INCLUDED
+#ifndef EVENT_H
+#define EVENT_H
 
+#include <iostream>
+using namespace std;
 
+struct Event {
+    string idEvent;
+    string namaEvent;
+    string lokasi;
+    string tanggal;
+    string jam;
+    int hargaTiket;
+};
 
-#endif // EVENT_H_INCLUDED
+typedef struct elEvent *adrEvent;
+
+struct elEvent {
+    Event info;
+    adrEvent next;
+};
+
+struct ListEvent {
+    adrEvent first;
+};
+
+void createEventList(ListEvent &LE);
+adrEvent newEvent(Event x);
+void insertLastEvent(ListEvent &LE, adrEvent E);
+adrEvent searchEvent(ListEvent LE, string id);
+bool isEventIDUsed(ListEvent LE, string id);
+void deleteEvent(ListEvent &LE, string id);
+
+void showAllEvent(ListEvent LE);
+
+#endif
