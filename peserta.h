f86e64acf9aa5c901bb6bc17c75c05320b9ffaf2
eLnae <nael200906@gmail.com>
2025-12-02 11:06:32 +0700

    Update peserta.h

--- peserta.h
+++ peserta.h
@@ -1,6 +1,34 @@
-#ifndef PESERTA_H_INCLUDED
-#define PESERTA_H_INCLUDED
+#ifndef PESERTA_H
+#define PESERTA_H
 
+#include <iostream>
+using namespace std;
 
+struct Peserta {
+    string idPeserta;
+    string nama;
+    string telepon;
+    string jenisKelamin;
+    string jenisPeserta;
+};
 
-#endif // PESERTA_H_INCLUDED
+typedef struct elPeserta *adrPeserta;
+
+struct elPeserta {
+    Peserta info;
+    adrPeserta next;
+};
+
+struct ListPeserta {
+    adrPeserta first;
+};
+
+void createPesertaList(ListPeserta &LP);
+adrPeserta newPeserta(Peserta x);
+void insertLastPeserta(ListPeserta &LP, adrPeserta P);
+void deletePeserta(ListPeserta &LP, string nama);
+bool isPesertaIDUsed(ListPeserta LP, string id);
+
+void showAllPeserta(ListPeserta LP);
+
+#endif
