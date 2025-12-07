fad1f65e2df097c2d49661a2bb330d21b82e6b68
eLnae <nael200906@gmail.com>
2025-12-08 00:50:51 +0700

    Implement peserta list management functions

--- peserta.cpp
+++ peserta.cpp
@@ -24,14 +24,39 @@ void insertLastPeserta(ListPeserta &LP, adrPeserta P) {
 }
 
 bool isPesertaIDUsed(ListPeserta LP, string id) {
-
+    adrPeserta P = LP.first;
+    while (P != NULL) {
+        if (P->info.idPeserta == id) return true;
+        P = P->next;
+    }
+    return false;
 }
 
 void deletePeserta(ListPeserta &LP, string nama) {
+    adrPeserta P = LP.first;
+    adrPeserta prev = NULL;
 
+    while (P != NULL && P->info.nama != nama) {
+        prev = P;
+        P = P->next;
+    }
+
+    if (P != NULL) {
+        if (prev == NULL) LP.first = P->next;
+        else prev->next = P->next;
+        delete P;
+    }
 }
 
 void showAllPeserta(ListPeserta LP) {
-
+    adrPeserta P = LP.first;
+    while (P != NULL) {
+        cout << "ID Peserta : " << P->info.idPeserta << endl;
+        cout << "Nama       : " << P->info.nama << endl;
+        cout << "Telepon    : " << P->info.telepon << endl;
+        cout << "Jenis Kel  : " << P->info.jenisKelamin << endl;
+        cout << "Jenis      : " << P->info.jenisPeserta << endl;
+        cout << "---------------------------" << endl;
+        P = P->next;
+    }
 }
-
