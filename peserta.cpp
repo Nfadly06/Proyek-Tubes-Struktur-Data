d320a4aa12b646a495970af21c595b0c487e1c80
eLnae <nael200906@gmail.com>
2025-12-20 14:52:36 +0700

    Update peserta.cpp

--- peserta.cpp
+++ peserta.cpp
@@ -1,16 +1,20 @@
 #include "peserta.h"
 
+/* CREATE LIST */
 void createPesertaList(ListPeserta &LP) {
     LP.first = NULL;
 }
 
+/* CREATE NODE */
 adrPeserta newPeserta(Peserta x) {
     adrPeserta P = new elPeserta;
     P->info = x;
+    P->prev = NULL;
     P->next = NULL;
     return P;
 }
 
+/* INSERT LAST (DLL) */
 void insertLastPeserta(ListPeserta &LP, adrPeserta P) {
     if (LP.first == NULL) {
         LP.first = P;
@@ -20,43 +24,55 @@ void insertLastPeserta(ListPeserta &LP, adrPeserta P) {
             Q = Q->next;
         }
         Q->next = P;
+        P->prev = Q;
     }
 }
 
+/* VALIDASI ID PESERTA */
 bool isPesertaIDUsed(ListPeserta LP, string id) {
     adrPeserta P = LP.first;
     while (P != NULL) {
-        if (P->info.idPeserta == id) return true;
+        if (P->info.idPeserta == id) {
+            return true;
+        }
         P = P->next;
     }
     return false;
 }
 
+/* DELETE PESERTA BERDASARKAN NAMA (DLL) */
 void deletePeserta(ListPeserta &LP, string nama) {
     adrPeserta P = LP.first;
-    adrPeserta prev = NULL;
 
     while (P != NULL && P->info.nama != nama) {
-        prev = P;
         P = P->next;
     }
 
     if (P != NULL) {
-        if (prev == NULL) LP.first = P->next;
-        else prev->next = P->next;
+        if (P->prev != NULL) {
+            P->prev->next = P->next;
+        } else {
+            LP.first = P->next;
+        }
+
+        if (P->next != NULL) {
+            P->next->prev = P->prev;
+        }
+
         delete P;
     }
 }
 
+/* SHOW ALL PESERTA */
 void showAllPeserta(ListPeserta LP) {
     adrPeserta P = LP.first;
     while (P != NULL) {
-        cout << "ID Peserta : " << P->info.idPeserta << endl;
-        cout << "Nama       : " << P->info.nama << endl;
-        cout << "Telepon    : " << P->info.telepon << endl;
-        cout << "Jenis Kel  : " << P->info.jenisKelamin << endl;
-        cout << "Jenis      : " << P->info.jenisPeserta << endl;
-        cout << "---------------------------" << endl;
+        cout << "ID Peserta    : " << P->info.idPeserta << endl;
+        cout << "Nama          : " << P->info.nama << endl;
+        cout << "Telepon       : " << P->info.telepon << endl;
+        cout << "Jenis Kelamin : " << P->info.jenisKelamin << endl;
+        cout << "Jenis Peserta : " << P->info.jenisPeserta << endl;
+        cout << "----------------------------" << endl;
         P = P->next;
     }
 }
